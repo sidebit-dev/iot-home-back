72c5b98d5ea3c5c592b32d9056ae0d71a5dc0651
Miguel Arcanjo <miguel.arcanjo@outlook.com>
2026-09-16 18:16:31 -0300

    User delete

--- src/main/java/dev/sidebit/iot_home_back/services/UserService.java
+++ src/main/java/dev/sidebit/iot_home_back/services/UserService.java
@@ -26,4 +26,8 @@ public class UserService {
     public User insert(User obj){
         return repository.save(obj);
     }
+
+    public void delete(Integer id){
+        repository.deleteById(id);
+    }
 }
