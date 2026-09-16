b69a5b8ca9fa24a9f493c8c0a0a3ea22aaab382b
Miguel Arcanjo <miguel.arcanjo@outlook.com>
2026-09-16 11:46:43 -0300

    Service layer, component registration

--- src/main/java/dev/sidebit/iot_home_back/resources/UserResource.java
+++ src/main/java/dev/sidebit/iot_home_back/resources/UserResource.java
@@ -1,20 +1,35 @@
 package dev.sidebit.iot_home_back.resources;
 
+import dev.sidebit.iot_home_back.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.sidebit.iot_home_back.entities.User;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+
+	@Autowired
+	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1, "Miguel", "miguel@gmail.com", "123456", "11988887777", true);
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list;
+        list = service.findAll();
+        return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Integer id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
