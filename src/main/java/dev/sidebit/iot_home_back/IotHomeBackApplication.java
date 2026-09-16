3313fbaac3d2126287503ac17a832b835eda6657
Miguel Arcanjo <miguel.arcanjo@outlook.com>
2026-09-15 21:22:32 -0300

    Project created

---
+++ src/main/java/dev/sidebit/iot_home_back/IotHomeBackApplication.java
@@ -0,0 +1,13 @@
+package dev.sidebit.iot_home_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotHomeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotHomeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sidebit/iot_home_back/IotHomeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sidebit.iot_home_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotHomeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
